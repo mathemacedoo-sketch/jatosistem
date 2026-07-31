1f53f96817c7ca4b68526e9750cbc3bda2fb500b
mathemacedoo-sketch <mathemacedoo@gmail.com>
2026-07-30 23:25:48 -0300

    subindo

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,10 +1,6 @@
-import { supabase } from "./lib/supabase";
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error("As variáveis do Supabase não foram configuradas.");
-}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
